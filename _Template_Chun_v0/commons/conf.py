9955be3f4404b73442877ef96bcb41c5283aed02
OrangePie <30658813+OrangePie-C@users.noreply.github.com>
2021-10-18 14:39:59 +0900

    Delete _Template_Chun_v0 directory

--- _Template_Chun_v0/commons/conf.py
+++
@@ -1,3 +0,0 @@
-dataroot = '/data/private/pretrainedmodels'
-imagenet_path = '/data/public/rw/datasets/imagenet-pytorch'
-sodeep_model = '/data/private/repos/sodeep/weights/best_model_gruc.pth.tar'
--- _Template_Chun_v0/practice.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-# base configs
-parser.add_argument('-c', '--config', type=str, default='gpu')
-
-parser = parser.parse_args()
-print(parser.config)
